c90f324fafb5797431b38e7f2da1aa204061bc97
KJNanika <KJGriffin2001@gmail.com>
2022-10-13 20:38:35 +1100

    first push

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+alert('WARNING! This site is still a work in progress, so there may be features that are either missing or buggy. Proceed with caution.  -Kira')
+console.log('Hello console!')
+let message ="Hello!";
+console.log(message);
+
+/*this is a comment*/
