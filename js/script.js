88c8cc43f44fa7969ac85b26f07615cebfce33d7
KJNanika <KJGriffin2001@gmail.com>
2022-10-14 15:37:26 +1100

    final push for Lesson 1

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ alert('WARNING! This site is still a work in progress, so there may be features 
 console.log('Hello console!')
 let message ="Hello!";
 console.log(message);
+console.log(20+5)
 
 /*this is a comment*/
