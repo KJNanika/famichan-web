eacb67d5f29ad288bb40485ac80045f3157eef58
Kira Griffin <112544971+KJNanika@users.noreply.github.com>
2022-11-16 12:44:39 +1100

    fixed fadein animation

--- js/script.js
+++ js/script.js
@@ -26,7 +26,13 @@ console.log ('Current location is ' + location.href);
 console.log('Current port number is ' + location.port);
 console.log(history);
 
-document.getElementById("KV").style.animation = 'scaler 1s ease-out';
+function kvFadeIn() {
+    document.getElementById("KV").style.display = 'flex';
+}
+
+setTimeout(kvFadeIn, 3000);
+
+//I would do <document.getElementById("KV").style.animation = "scaler 1s ease-out"> here in my JS, but I only ended up breaking the code that way so I'll just do it directly in CSS instead. Hey, at least I tried.
 
 
 let socialID = document.getElementById('media');
